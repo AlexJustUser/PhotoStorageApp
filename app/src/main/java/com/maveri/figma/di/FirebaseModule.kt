fab7978780ec9c7a5a142a0d12d76c54737bd183
alex <swimmersuper16@gmail.com>
2021-10-13 18:19:31 +0300

    Adding layouts for displaying locations.

---
+++ app/src/main/java/com/maveri/figma/di/FirebaseModule.kt
@@ -0,0 +1,19 @@
+package com.maveri.figma.di
+
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+
+@Module
+@InstallIn(ViewModelComponent::class)
+object FirebaseModule {
+    @Provides
+    fun provideFirebaseDatabase() = FirebaseDatabase.getInstance()
+
+    @Provides
+    fun provideFirebaseAuth() = Firebase.auth
+}
